ac2c48517789e19d15c1a63307df2de52f80037c
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-14 10:16:46 +0200

    Add: Function show hide answer

--- JS/card.js
+++ JS/card.js
@@ -1,9 +1,28 @@
 console.clear();
 
+//-----------------Bookmark Toggle-----------------------
+
 const bookmark = document.querySelector('[data-js="bookmark"]');
 bookmark.addEventListener("click", toggleBookmark);
 
 function toggleBookmark() {
   bookmark.classList.toggle("card__bookmark-active");
   console.log("test");
+}
+
+//-----------------Answer Toggle-----------------------
+
+const answerButton = document.querySelector('[data-ja="answerButton"]');
+const quizAnswer = document.querySelector('[data-js="quizAnswer"]');
+answerButton.addEventListener("click", toggleAnswer);
+
+function toggleAnswer() {
+  quizAnswer.toggleAttribute("hidden");
+  answerButton.classList.toggle("card__button--hide");
+
+  if (quizAnswer.hasAttribute("hidden")) {
+    answerButton.textContent = "Show answer";
+  } else {
+    answerButton.textContent = "Hide answer";
+  }
 }
