e2b122c637ef7e08af4ad511b70e2a0df13d2b1a
Anna Schemmel <anna.schemmel@gmx.de>
2025-08-15 10:03:57 +0200

    Fix: Toggle functions on bookmark page

--- JS/card.js
+++ JS/card.js
@@ -8,28 +8,21 @@ import { toggleAnswer } from "./helpers.js";
 
 const bookmark = document.querySelector('[data-js="bookmark"]');
 
-// In the event we need to pass a "our" variable
+// In the event we need to pass "our" variable
 bookmark.addEventListener("click", () => toggleBookmark(bookmark));
 
 // bookmark.addEventListener("click", toggleBookmarkEvent);
-
 // function toggleBookmark(bookmarkElement) {
 //   bookmarkElement.classList.toggle("card__bookmark-active");
 //   console.log("test");
 // }
 // ^ this was written before we exported the function to the "helpers" file
 
-// bookmark.addEventListener("click", () => {
-//   bookmark.classList.toggle("card__bookmark-active");
-//   console.log("test");
-// });
-// in this case
-
 //-----------------Answer Toggle-----------------------
 
 // can be exported to the "helpers" file once done with the other tasks
 
-const answerButton = document.querySelector('[data-ja="answerButton"]');
+const answerButton = document.querySelector('[data-js="answerButton"]');
 const quizAnswer = document.querySelector('[data-js="quizAnswer"]');
 
 console.log(answerButton);
--- JS/helpers.js
+++ JS/helpers.js
@@ -1,15 +1,14 @@
+// -------------------------Bookmark toggle-------------------------
+
 export function toggleBookmark(bookmark) {
   bookmark.classList.toggle("card__bookmark-active");
-  console.log("test");
+  //console.log("test");
 }
 
-// export function toggleBookmarkEvent(event) {
-//   event.target.classList.toggle("card__bookmark-active");
-//   console.log("test");
-// }
-
+// -------------------------answer button toggle-------------------------
 export function toggleAnswer(answerButton, quizAnswer) {
   quizAnswer.toggleAttribute("hidden");
+  //quizAnswer.classList.toggle("card__answer--hidden");
   answerButton.classList.toggle("card__button--ghost");
 
   if (quizAnswer.hasAttribute("hidden")) {
